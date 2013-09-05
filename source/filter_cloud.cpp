917da93a4def045036081401503d1457e26d19bf
Juan Manuel <jmruiz@cio.mx>
2013-09-04 19:17:41 -0500

    Create base file for filter_cloud.cpp

--- source/filter_cloud.cpp
+++ source/filter_cloud.cpp
@@ -2,7 +2,26 @@
 
 using namespace std;
 
-int main(){
-    cout << "Hi!!" << endl;
+int main (int argc, char** argv){
+
+    cout << "\n Usage: "<< argv[0] <<"\n"
+         << "---------------------------------------------------------\n"
+         << " Press:\n"
+         << "  - Q      - to close the viewer and exit the program\n"
+         << "  - L      - to show the list of filter availables\n"
+         << "---------------------------------------------------------\n"
+         << endl;
+/*
+    string device_id("");
+    pcl::OpenNIGrabber::Mode depth_mode = pcl::OpenNIGrabber::OpenNI_Default_Mode;
+    pcl::OpenNIGrabber::Mode image_mode = pcl::OpenNIGrabber::OpenNI_Default_Mode;
+
+    // Create a new OpenNiGrabber
+    pcl::OpenNIGrabber grabber(device_id, depth_mode, image_mode);
+
+    //OpenNIViewer<pcl::PointXYZRGBA> openni_viewer(grabber);
+    OpenNiViewer openni_viewer(grabber);
+    openni_viewer.run();
+*/
     return 0;
 }
