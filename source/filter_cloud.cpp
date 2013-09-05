72162958729d5c4869db57bc7ca77e7180a72e7f
Juan Manuel <jmruiz@cio.mx>
2013-09-05 18:49:05 -0500

    Filter_Cloud: Checks the given parameter - Must be a valid "pcd" file name

--- source/filter_cloud.cpp
+++ source/filter_cloud.cpp
@@ -1,21 +1,55 @@
 #include <iostream>
+#include <string>
+
+void printUsage(char* pname){
+    std::cout << "\n Usage: "<< pname << "  point_cloud_file.pcd" <<"\n"
+              << "---------------------------------------------------------\n"
+              << " Press:\n"
+              << "  - ENTER    - to save the filtered Point Cloud\n"
+              << "  - Q        - to close the viewer and exit the program\n"
+              << "  - 1 - 9    - to apply a filter  \n"
+              << "---------------------------------------------------------\n"
+              << std::endl;
+}
 
 int
 main (int argc, char** argv)
 {
     // Check number of parameters
-    std::cout << argc << std::endl;
     if(argc < 2 || argc > 2){
-        std::cout << "\n Usage: "<< argv[0] << "  point_cloud_file.pcd" <<"\n"
-                  << "---------------------------------------------------------\n"
-                  << " Press:\n"
-                  << "  - ENTER    - to save the filtered Point Cloud\n"
-                  << "  - Q        - to close the viewer and exit the program\n"
-                  << "  - 1 - 9    - to apply a filter  \n"
-                  << "---------------------------------------------------------\n"
-                  << std::endl;
+        std::cout << " Error: This program needs a parameter to work " << std::endl;
+        printUsage(argv[0]);
         return 1;
     }
 
-      return (0);
+    // Check the filename:
+    std::string parameter = argv[1];
+    size_t position_point = parameter.find(".");
+    std::string extension;
+    std::string filename;
+
+    // Check that filename has a point
+    if( position_point == parameter.npos ){
+        std::cout << " Error: The given parameter \"" << parameter << "\" must have a valid file name!" << std::endl;
+        printUsage(argv[0]);
+        return 1;
+    }
+
+    extension = parameter.substr(position_point+1, parameter.length());
+    filename = parameter.substr(0, position_point);
+
+    // Check that file name has a "*.pcd" extension
+    if( extension.compare("pcd") ){
+        std::cout << " Error: The given parameter \"" << parameter << "\" isn't a \"*.pcd\" file!" << std::endl;
+        printUsage(argv[0]);
+        return 1;
+    }
+
+    std::cout << "Input Parameter: " << parameter << std::endl;
+    std::cout << "Filename: " << filename << std::endl;
+    std::cout << "Extension: " << extension << std::endl;
+
+    // Class of functions for filters
+
+    return (0);
 }
