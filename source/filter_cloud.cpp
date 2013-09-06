827a45bf9433c60acd1973fc27cefb549b523b0b
Juan Manuel <jmruiz@cio.mx>
2013-09-05 19:21:22 -0500

    Filter_Cloud: Added Read function for pcd files Fixed bugs: - Find ".pcd" instead "." to avoid "directory jumps" in the given parameter

--- source/filter_cloud.cpp
+++ source/filter_cloud.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <pcl/io/pcd_io.h>
+
 
 void printUsage(char* pname){
     std::cout << "\n Usage: "<< pname << "  point_cloud_file.pcd" <<"\n"
@@ -12,9 +14,7 @@ void printUsage(char* pname){
               << std::endl;
 }
 
-int
-main (int argc, char** argv)
-{
+int main (int argc, char** argv){
     // Check number of parameters
     if(argc < 2 || argc > 2){
         std::cout << " Error: This program needs a parameter to work " << std::endl;
@@ -24,19 +24,19 @@ main (int argc, char** argv)
 
     // Check the filename:
     std::string parameter = argv[1];
-    size_t position_point = parameter.find(".");
+    size_t position_ext = parameter.find(".pcd");
     std::string extension;
     std::string filename;
 
     // Check that filename has a point
-    if( position_point == parameter.npos ){
+    if( position_ext == parameter.npos ){
         std::cout << " Error: The given parameter \"" << parameter << "\" must have a valid file name!" << std::endl;
         printUsage(argv[0]);
         return 1;
     }
 
-    extension = parameter.substr(position_point+1, parameter.length());
-    filename = parameter.substr(0, position_point);
+    extension = parameter.substr(position_ext+1, parameter.length());
+    filename = parameter.substr(0, position_ext);
 
     // Check that file name has a "*.pcd" extension
     if( extension.compare("pcd") ){
@@ -49,7 +49,14 @@ main (int argc, char** argv)
     std::cout << "Filename: " << filename << std::endl;
     std::cout << "Extension: " << extension << std::endl;
 
-    // Class of functions for filters
+    // Functions for filters
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+
+    // Read the input cloud data
+    pcl::PCDReader reader;
+    reader.read<pcl::PointXYZ> (parameter, *cloud_in);
+
 
     return (0);
 }
