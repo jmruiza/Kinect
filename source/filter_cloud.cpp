50ba006389587c0fea27bd89edd98bc963afc33f
Juan Manuel <jmruiz@cio.mx>
2013-09-06 20:39:23 -0500

    Filter_Cloud: - Read / Write PCD files ready! - Keyboard Callbacks ready!
    - Apply filters "live" dont work <- Working it

--- source/filter_cloud.cpp
+++ source/filter_cloud.cpp
@@ -1,36 +1,5 @@
 #include <iostream>
 #include "filters_viewer.h"
-//#include <string>
-//#include <pcl/io/pcd_io.h>
-//#include <pcl/point_types.h>
-//#include <pcl/visualization/pcl_visualizer.h>
-//#include <pcl/filters/statistical_outlier_removal.h>
-
-//boost::shared_ptr<pcl::visualization::PCLVisualizer>
-//viewportsVis ( pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in,
-//               pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out){
-
-//    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
-//    viewer->initCameraParameters ();
-
-//    int v1(0);
-//    viewer->createViewPort(0.0, 0.0, 0.5, 1.0, v1);
-//    viewer->setBackgroundColor (0, 0, 0, v1);
-//    viewer->addText("Before..", 10, 10, "v1 text", v1);
-//    viewer->addPointCloud<pcl::PointXYZ> (cloud_in, "sample cloud1", v1);
-
-//    int v2(0);
-//    viewer->createViewPort(0.5, 0.0, 1.0, 1.0, v2);
-//    viewer->setBackgroundColor (0.3, 0.3, 0.3, v2);
-//    viewer->addText("After..", 10, 10, "v2 text", v2);
-//    viewer->addPointCloud<pcl::PointXYZ> (cloud_out, "sample cloud2", v2);
-
-//    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud1");
-//    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud2");
-//    viewer->addCoordinateSystem (1.0);
-
-//    return (viewer);
-//}
 
 void printUsage(char* pname){
     std::cout << "\n Usage: "<< pname << "  point_cloud_file.pcd" <<"\n"
@@ -81,43 +50,6 @@ int main (int argc, char** argv){
     FiltersViewer fv;
     fv.set_FileNames(filename);
     fv.run();
-/*
 
-
-    // Functions for filters
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in (new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
-
-    // Read the input cloud data
-    pcl::PCDReader reader;
-    reader.read<pcl::PointXYZ> (parameter, *cloud_in);
-
-    // Here include all the availabel filters - Check documentation
-
-
-    // Create the filtering object
-    pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
-    sor.setInputCloud (cloud);
-    sor.setMeanK (50);
-    sor.setStddevMulThresh (1.0);
-    sor.filter (*cloud_filtered);
-
-    pcl::PCDWriter writer;
-    writer.write<pcl::PointXYZ> ("table_scene_lms400_inliers.pcd", *cloud_filtered, false);
-
-    sor.setNegative (true);
-    sor.filter (*cloud_filtered);
-    writer.write<pcl::PointXYZ> ("table_scene_lms400_outliers.pcd", *cloud_filtered, false);
-
-
-    // Added by me
-    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
-    viewer = viewportsVis(cloud, cloud_filtered);
-
-    while (!viewer->wasStopped ()){
-        viewer->spinOnce (100);
-        boost::this_thread::sleep (boost::posix_time::microseconds (100000));
-    }
-*/
     return (0);
 }
--- source/filters_viewer.h
+++ source/filters_viewer.h
@@ -37,7 +37,7 @@ public:
         std::stringstream tmp1, tmp2;
         tmp1 << filename << ".pcd";
         st_cloud_in = tmp1.str();
-        tmp2 << filename << "_out.pcd";
+        tmp2 << filename << "_filtered.pcd";
         st_cloud_out = tmp2.str();
         /*// Print filenames (optional)
         std::cout << "Input: " << st_cloud_in << std::endl;
@@ -61,15 +61,25 @@ public:
     // Keyboard callback
     void keyboard_callback (const pcl::visualization::KeyboardEvent& event, void*){
         if ( event.keyDown() ){
+            std::cout << event.getKeySym() << std::endl;
             // Press ENTER key: Saves the filtered cloud
             if(event.getKeySym() == "Return"){
                 pcl::PCDWriter writer;
                 writer.write<pcl::PointXYZ> (st_cloud_out, *cloud_out_);
+                std::cout << "Saved: " << st_cloud_out << std::endl;
             }
             // Press ESCAPE key: Do nothing
             if(event.getKeySym() == "Escape"){
-                std::cout << event.getKeySym() << " key was pressed, to stop, press Q key" << std::endl;
+                viewer_->close();
+                // std::cout << event.getKeySym() << " key was pressed, to stop, press Q key" << std::endl;
             }
+
+            // Press S: Statistical Outlier Removal filter
+            if(event.getKeySym() == "S" || event.getKeySym() == "s"){
+                std::cout << " -> Statistical Outlier Removal filter" << std::endl;
+                fil_StatisticalOutlierRemoval();
+            }
+
         }
     }
 
@@ -78,39 +88,43 @@ public:
         init_Visualizer();
         while (!viewer_->wasStopped ()){
             viewer_->spinOnce (100);
-            boost::this_thread::sleep (boost::posix_time::microseconds (100000));
+            boost::this_thread::sleep (boost::posix_time::microseconds (100));
         }
     }
 
-//    // Function for Statistical Outlier Removal filter:
-//    void fil_StatisticalOutlierRemoval(bool negative=false){
-//        // Create filtering object
-//        pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
-//        // Parameters
-//        sor.setInputCloud (cloud_in_);
-//        sor.setMeanK (50);
-//        sor.setStddevMulThresh (1.0);
-//        sor.setNegative(negative);
-//        // Output
-//        sor.filter (*cloud_out_);
+    // Function for Statistical Outlier Removal filter:
+    void fil_StatisticalOutlierRemoval(bool negative=false){
+        // Create filtering object
+        pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+        // Parameters
+        sor.setInputCloud (cloud_in_);
+        sor.setMeanK (50);
+        sor.setStddevMulThresh (1.0);
+        sor.setNegative(negative);
+        // Output
+        sor.filter (*cloud_out_);
 
-//        // Update visualizer
-//        // viewer = viewportsVis(cloud_in_, cloud_out_);
-//    }
+        // Reset visualizer
+
+        // Update visualizer
+        viewer_ = viewportsVis(cloud_in_, cloud_out_);
+    }
 
 private:    
 
     boost::shared_ptr<pcl::visualization::PCLVisualizer> viewportsVis
     ( pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in, pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out){
 
-        boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+        boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("Point cloud"));
         viewer->initCameraParameters ();
+        viewer->registerKeyboardCallback(&FiltersViewer::keyboard_callback, *this);
 
         int v1(0);
         viewer->createViewPort(0.0, 0.0, 0.5, 1.0, v1);
         // viewer->setBackgroundColor (0, 0, 0, v1);
         viewer->addText("Without Filter...", 10, 10, "v1 text", v1);
         viewer->addPointCloud<pcl::PointXYZ> (cloud_in, "sample cloud1", v1);
+
 
         int v2(0);
         viewer->createViewPort(0.5, 0.0, 1.0, 1.0, v2);
@@ -120,6 +134,7 @@ private:
 
         viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud1");
         viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud2");
+        viewer->resetCameraViewpoint("Point cloud");
         // viewer->addCoordinateSystem (1.0);
 
         return (viewer);
