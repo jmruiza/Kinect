2528900453769363c8789e5f0e9156b6eba8e21e
Juan Manuel <jmruiz@cio.mx>
2013-10-04 00:21:56 -0500

    Heights: Validate input parameters pending

--- source/heights.cpp
+++ source/heights.cpp
@@ -5,25 +5,42 @@ void printUsage(char* pname){
     std::cout << "\n Usage: "<< pname << " [file name] [unit of measure] [absolute measure]"
               << "\n Example: ./Heights ../../filename true true"
               << "\n ------------------------------------------------------------------"
+              << "\n unit of measure    -   meters (true or null)"
+              << "\n                    -   centimeters (false)"
+              << "\n absolute measure   -   absolute (true or null) "
+              << "\n                    -   relative (false)"
               << std::endl;
 }
 
 int main (int argc, char** argv){
-    // Number of parameters needed
-    int n_params = 3;
 
     // Check number of parameters
-    if(argc < n_params+1 || argc > n_params+1){
-        std::cout << " Error: This program needs " << n_params << " parameter(s) to work" << std::endl;
+    if(argc < 2 || argc > 4){
+        std::cout << " Error: This program needs 1 or 3 parameter(s) to work" << std::endl;
+        std::cout << argc << std::endl;
         printUsage(argv[0]);
         return (1);
     }
 
-    // Set the filename
-    Heights height(argv[1]);
-    height.distanceInMeters(false);
-    height.distanceAbsolute(false);
-    height.run();
+    if(argc == 2){
+        Heights height(argv[1]);
+        height.run();
+    }
+
+    if(argc == 3){
+        Heights height(argv[1]);
+        height.distanceInMeters(argv[2]);
+        height.run();
+    }
+
+    if(argc == 4){
+        Heights height(argv[1]);
+        height.distanceInMeters(argv[2]);
+        height.distanceAbsolute(argv[3]);
+        height.run();
+    }
+
+    return 0;
 }
 
 /* // Debug mode
